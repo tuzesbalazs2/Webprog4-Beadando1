c3743242e6f79a4124c5b3822f75d16178cf8d05
tuezesdaaniel <tuezesdaaniel@gmail.com>
2019-11-08 21:37:14 +0100

    data.txt meg js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 try{
 $.get('https://raw.githubusercontent.com/tuzesbalazs2/Webprog4-Beadando1/master/data.txt', function(data) {
 //alert(data);
-   document.querySelector(".card-body p").innerHTML=data;
+   $(".card-body p").html(data);
 }, 'text');
 } catch (err){alert(err.message);}
